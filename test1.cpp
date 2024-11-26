f2ecaa75a19b6c8ba8de4d1a0b4e1cbde87c4b43
6704062611085 <s6704062611085@email.kmutnb.ac.th>
2024-11-26 14:58:21 +0700

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -3,12 +3,18 @@
 int main() {
 	int n1;
 	int n2;
-	n1 = 1;
-	n2 = 2;
+	printf("inter to n1");
+	scanf("%d",&n1);
+	printf("inter to n2");
+	scanf("%d",&n2);
 	if ( n2 > n1 ) {
 		printf("n2 > n1");
+		printf("\n%d",(n2-n1));
+		//diff of n2 > n1
 	} else{
 		printf("n1 > n2");
+		printf("\n%d",(n1-n2));
+		//diff of n1 > n2
 	}
 	return 0;
 }
