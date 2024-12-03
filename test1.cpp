ef85f24307dc25375f934cac8aaad39da33f13dc
6704062611085 <s6704062611085@email.kmutnb.ac.th>
2024-12-03 13:19:35 +0700

    เเก้ไข้ ข้อที่ 1 ใหม่
    ผมได้ทำการปรับเเก้ไข้ จากคร้งที่ ผมได้ทำนั้น ผมได้ใช่ if else ผมได้ปรับมาเป็น if if

--- test1.cpp
+++ test1.cpp
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 int main(){
-	int n1;
-	int n2;
-	printf("enter to number");
-	scanf("%d",&n1);
-	printf("enter to number");
-	scanf("%d",&n2);
+	int n1 = 10;
+	int n2 = 20;
 	if (n1 > n2){
 		printf("n1 > n2");
 		printf("\n%d",(n1 - n2));
-	}else{
+	}if( n2 > n1){
 		printf("n2 > n1");
 		printf("\n%d",(n2 - n1));
 	}
