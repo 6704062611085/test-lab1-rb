d79d25a248c6f060df29a373d56f87945c8f643c
6704062611085 <s6704062611085@email.kmutnb.ac.th>
2024-12-03 13:02:17 +0700

    ข้อที่ 3 คับผม
    ที่เพิ่มเติมขึ้น มาคือการที่ ผท n1 n2 ไปรวมกันในบรรทัดเดียวกันคับ
    เเละทำให้การให้ code นั้นสั้นมากยิ่งขึ้น

--- test1.cpp
+++ test1.cpp
@@ -1,22 +1,18 @@
 #include <stdio.h>
 
-int main() {
-	int n1;
-	int n2;
-	printf("inter to n1");
+int main(){
+	int n1,n2;
+	printf("enter to number");
 	scanf("%d",&n1);
-	printf("inter to n2");
+	printf("enter to number");
 	scanf("%d",&n2);
-	if ( n2 > n1 ) {
-		printf("n2 > n1");
-		printf("\n%d",(n2-n1));
-		//diff of n2 > n1
-	} else{
+	if (n1 > n2){
 		printf("n1 > n2");
-		printf("\n%d",(n1-n2));
-		//diff of n1 > n2
+		printf("\n%d",(n1 - n2));
+	}else{
+		printf("n2 > n1");
+		printf("\n%d",(n2 - n1));
 	}
+	
 	return 0;
 }
-
-
